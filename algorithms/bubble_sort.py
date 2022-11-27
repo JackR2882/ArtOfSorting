13714f48408f1092b045281947cff8d6737b78ec
JackR223 <sgjrive@liverpool.ac.uk>
2022-11-27 16:43:20 +0000

    temporary implmentation of audio for testing

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,13 +1,21 @@
 # basic implementation of bubble sort:
 
+import audio_controller_temp
+
 unsorted = True
 #unsorted = False
 
 def sort(obj):
+    
+    audio = audio_controller_temp.AudioOut()
+
     unsorted = True
     while unsorted:
         unsorted = False
         for i in range(1, len(obj.stripState)):
+
+            audio.out(obj.stripState[i][0])
+
             currVal = obj.stripState[i]
             prevVal = obj.stripState[i-1]
             obj.stripState[i] = [0,255,255,255,255]
