bc551b987b28fe433cc305abe35d4ddd5a94dc93
yJackR223 <sgjrive@liverpool.ac.uk>
2022-12-19 11:54:42 +0000

    added audio to insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,10 +1,16 @@
+import audio_controller_temp
+
 #basic implmentation of insertion sort:
 def sort(obj):
 
+    audio = audio_controller_temp.AudioOut()
+
     sorted = 1
     for i in range(0, len(obj.stripState)-1):
+        audio.out(obj.stripState[i][0])
         for n in reversed(range(0,sorted)):
             if obj.stripState[n] > obj.stripState[n+1]:
+                audio.out(obj.stripState[n][0])
                 mem = obj.stripState[n+1]
                 obj.stripState[n+1] = [0,255,255,255,255]
                 obj.update()
