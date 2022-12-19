7146181135bae70d603fcc77b8b1c167b35791fc
yJackR223 <sgjrive@liverpool.ac.uk>
2022-12-19 11:54:16 +0000

    improved clipping on audio

--- audio_controller_temp.py
+++ audio_controller_temp.py
@@ -7,7 +7,8 @@ import pyaudio
 class AudioOut:
     def __init__(self):
         self.sampleRate = 44000
-        self.duration = 0.00001
+        #self.duration = 0.00001
+        self.duration = 0.05
         self.sineFreq = 440.0
         self.volume = 1.0
 
@@ -24,8 +25,28 @@ class AudioOut:
 
     def out(self, val):
         val = val/72+0.5
-        ordered = (np.sin(val*np.pi * np.arange(self.sampleRate * self.duration) * self.sineFreq / self.sampleRate)).astype(np.float32)
+        ordered = (np.sin(val*np.pi * np.arange((self.sampleRate/2) * (self.duration/2)) * self.sineFreq / self.sampleRate)).astype(np.float32)
+        
+        ordered = np.concatenate((ordered,np.flip(ordered)), axis=None)
+
         output = (self.volume * ordered).tobytes()
+        
+        
+        #ordered = np.arange((self.sampleRate/2) * (self.duration/2)) # produces array of values in range 0 to (sampleRate*duration)-1
+
+        # rescale these values:
+        #ordered  = (ordered * self.sineFreq / self.sampleRate)*val
+
+        #ordered = np.concatenate((ordered,np.flip(ordered)), axis=None)
+
+        #print(ordered)
+        #ordered = np.array([0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+        #                    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+        #                    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+        #                    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+        #                    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+        #                    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5])
+        #output = (self.volume * ordered).tobytes()
         self.stream.write(output)
 
 
