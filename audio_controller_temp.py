231afcf3ef6d9fcf29d16bfe8525a3a8a4949d1d
yJackR223 <sgjrive@liverpool.ac.uk>
2022-12-28 11:41:19 +0000

    formatting

--- audio_controller_temp.py
+++ audio_controller_temp.py
@@ -6,11 +6,21 @@ import pyaudio
 
 class AudioOut:
     def __init__(self):
+        #self.sampleRate = 40000
+
         self.sampleRate = 44000
+        #self.sampleRate = 22000
+
         #self.duration = 0.00001
-        self.duration = 0.05
+        self.duration = 0.5
+        #self.duration = 0.05
+
+        # can get interesting effects by changing this
+        self.sineFreq = 110.0
         self.sineFreq = 440.0
-        self.volume = 1.0
+
+        # lowering this reduces clipping
+        self.volume = 0.5
 
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paFloat32,
