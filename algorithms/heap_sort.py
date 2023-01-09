16ffd059a92fb368a68bdf2d67fceb51098e3d96
JackR223 <sgjrive@liverpool.ac.uk>
2023-01-09 12:46:38 +0000

    heap sort integrated with led strip

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,4 +1,6 @@
 import math
+# import time
+
 # need to convert to use min heap rather than max heap,
 # in order to sort min-max rather than max-min
 
@@ -12,47 +14,6 @@ import math
 # To heapify subtree rooted at index i.
 # n is size of heap
 
-#arr = [4,3,7,2,8,9,1]
-#arr = [42,29,18,14,11,13,18,7,12,100]
-
-
-#def insert_heap(temp_heap, item, i):
-
-#	heap_update = temp_heap
-
-	# parent = i/2
-	# left_child = (2*index) + 1
-	# right_child = (2*index) + 2
-
-#	parent_i = math.floor(i/2)
-
-#	if i/2 >= 1:
-#		while i/2 >= 1:
-#			parent_i = math.floor(i/2)-1
-#			if item > heap_update[parent_i]:
-#				heap_update[i] = heap_update[parent_i]
-#				heap_update[parent_i] = item
-#			i = parent_i
-#	else:
-#		heap_update[i] = item
-#
-#	print(heap_update)
-#	return(heap_update)
-#
-#def heap(arr_in):
-#
-#	length = len(arr_in)
-#	temp_heap = [0]*length
-#	for i in range(0,length):
-#		temp_heap = insert_heap(temp_heap, arr_in[i], i)
-#
-#	return temp_heap
-
-arr = [4,3,7,2,8,9,1]
-arr = [42,29,18,14,11,13,18,7,12,100]
-
-
-
 
 
 # seems to work but is relatively slow
@@ -60,19 +21,15 @@ def heapify(arr_in, i):
 
 	parent = math.floor(((i+1)/2)-1)
 
-	#print("index: " + str(i), "parent: " + str(parent), "     arr_in[parent]: " + str(arr_in[parent]), "     arr_in[i]: " + str(arr_in[i]))
 	if i >= 0:
 
 		x = i
 		# bubble up as far as possible
-		while parent >= 0:
-							
+		while parent >= 0:			
 			if (arr_in[parent] < arr_in[x]):
-				#print("x: " + str(x), "parent: " + str(parent), "     arr_in[parent]: " + str(arr_in[parent]), "     arr_in[x]: " + str(arr_in[x]))
 
 				# swap (bubble up)
 				arr_in[x], arr_in[parent] = arr_in[parent], arr_in[x]
-				#arr_in = heapify(arr_in, i)
 
 				arr_in = heapify(arr_in, x)
 
@@ -86,43 +43,21 @@ def heapify(arr_in, i):
 	
 	return arr_in
 
-print(heapify(arr, len(arr)-1))
+def sort(obj):
+	arr = obj.stripState
 
-
-
-# seems to work, but as above not sure about optimality
-def heap_sort_test(arr_in):
-	heaped_arr = heapify(arr_in, len(arr_in)-1)
-	sorted_arr = [0]*len(heaped_arr)
-
-	i = 0
-
-	while i <= len(heaped_arr)-1:
-
-		#print(i)
-		sorted_arr[i] = heaped_arr[0]
-		heaped_arr[0], heaped_arr[len(heaped_arr)-(1+i)] = heaped_arr[len(heaped_arr)-(1+i)], 0
-		heaped_arr = heapify(heaped_arr, len(heaped_arr)-1)
-
-		i += 1
-
-	return sorted_arr
-
-print(heap_sort_test(arr))
-
-def heap_sort(obj):
-	obj.stripState = heapify(obj.stripState,len(obj.stripState)-1)
+	obj.stripState = heapify(arr,len(arr)-1)
 	obj.update()
 
 	heaped_arr = obj.stripState
-	sorted_arr = [0]*len(heaped_arr)
+	sorted_arr = [[0,0,0,0,0]]*len(heaped_arr)
 
 	i = 0
 
 	while i <= len(heaped_arr)-1:
 
-		sorted_arr = heaped_arr[0]
-		heaped_arr[0], heaped_arr[len(heaped_arr)-(1+i)] = heaped_arr[len(heaped_arr)-(1+i)], 0
+		sorted_arr[i] = heaped_arr[0]
+		heaped_arr[0], heaped_arr[len(heaped_arr)-(1+i)] = heaped_arr[len(heaped_arr)-(1+i)], [0,0,0,0,0]
 		heaped_arr = heapify(heaped_arr, len(heaped_arr)-1)
 
 		obj.stripState = sorted_arr
@@ -131,6 +66,26 @@ def heap_sort(obj):
 		i += 1
 
 	return sorted_arr
+
+
+
+# seems to work, but as above not sure about optimality
+#def heap_sort_test(arr_in):
+#	heaped_arr = heapify(arr_in, len(arr_in)-1)
+#	sorted_arr = [0]*len(heaped_arr)
+
+#	i = 0
+#
+#	while i <= len(heaped_arr)-1:
+
+		#print(i)
+#		sorted_arr[i] = heaped_arr[0]
+#		heaped_arr[0], heaped_arr[len(heaped_arr)-(1+i)] = heaped_arr[len(heaped_arr)-(1+i)], 0
+#		heaped_arr = heapify(heaped_arr, len(heaped_arr)-1)
+
+#		i += 1
+
+#	return sorted_arr
 
 
 
