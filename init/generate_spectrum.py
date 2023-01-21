002018ec689b5e1e1e41ffd2fa680a222ccf16f8
JackR223 <sgjrive@liverpool.ac.uk>
2023-01-21 08:58:11 +0000

    included option 2 generate spectrum from rand dist

--- init/generate_spectrum.py
+++ init/generate_spectrum.py
@@ -1,6 +1,11 @@
 #initialize() -> generates and sets pixel values to form an rgb spectrum
 
+#initializeHalfSpectrum() -> generates a random distribution of pixels between red and red+green - with random repeats
+#                            used in counting sort
+#                            (not sorted by default)
+ 
 import math
+import random
 
 #initialize spectrum of rgb colours
 def initialize(obj, stripSize, defaultBrightness):
@@ -14,4 +19,15 @@ def initialize(obj, stripSize, defaultBrightness):
         obj.setPixel(i+rampSize*3,i+rampSize*3,defaultBrightness,0,255-i*step,255)
         obj.setPixel(i+rampSize*4,i+rampSize*4,defaultBrightness,i*step,0,255)
         obj.setPixel(i+rampSize*5,i+rampSize*5,defaultBrightness,255,0,255-i*step)
+        i+=1
+
+
+def initializeHalfSpectrum(obj,stripSize,defaultBrightness):
+
+    i = 0
+    while i < stripSize:
+        rand = random.random()
+
+        # could round rand first to get a slightly more uniform dist? -> maybe 2d.p (gives higher chance of repeats)
+        obj.setPixel(int(rand*100),i,defaultBrightness,255,255*rand,0)
         i+=1
