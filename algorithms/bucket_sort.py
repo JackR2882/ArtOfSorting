09160f12cfe8d1cda76a0e4571fa6a9c85cb1047
JackR223 <sgjrive@liverpool.ac.uk>
2023-01-22 16:26:10 +0000

    refactoring

--- algorithms/bucket_sort.py
+++ algorithms/bucket_sort.py
@@ -11,25 +11,17 @@ def sort(obj):
     obj.update()
 
 
-    #arr = arr[0:20]
-    #print(len(arr))
-
+    # get max item from arr to decide what bucket size to use
     max = arr[0][0]
     for i in range(1, len(arr)):
         if max < arr[i][0]:
-            max = arr[i][0]
-    
+            max = arr[i][0]    
     max += 1
 
-    #print(max)
     bucket_range = math.ceil(max / num_buckets)
-    print(bucket_range)
-    #print(bucket_range)
 
-    # divide array into 5 buckets:        NEED TO THINK OF A WAY OF REPRESENTING THIS ON LED STRIP?
-
+    # divide array into 5 buckets:
     obj.clear()
-
     buckets = [[]]*num_buckets
     for i in range(0, len(arr)):
         bucket_index = math.floor(arr[i][0]/bucket_range)
@@ -40,9 +32,7 @@ def sort(obj):
         buckets[bucket_index] = temp_arr
 
 
-        obj.stripState = []
-        #obj.stripState = obj.stripState + buckets[0] + buckets[1] + buckets[2] + buckets[3] + buckets[4] + buckets[5] + buckets[6] + buckets[7] + buckets[8] + buckets[9]
-        
+        obj.stripState = []       
         for n in range (0, num_buckets):
             obj.stripState = obj.stripState + buckets[n]
 
@@ -50,10 +40,10 @@ def sort(obj):
         time.sleep(0.1)
 
 
-    # now just need to sort buckets - can just do basic sort - e.g. insertion sort
+    # now just need to sort buckets - can just do any basic sort e.g. insertion sort
     for i in range(0, num_buckets):
         arr_unsorted = buckets[i]
-        sorted = 1
+
         for x in range(1, len(arr_unsorted)):
             curr_pos = x
             # insert into right position (insertion sort)
@@ -70,12 +60,5 @@ def sort(obj):
             obj.update()
             time.sleep(0.1)
 
-            
-
-
-
-
-
 
     obj.clear()
-    time.sleep(2.5)
