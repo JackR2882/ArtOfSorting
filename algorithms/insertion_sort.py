e2884ab708a28aec777877b87b74cccdae99c522
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-02 09:44:59 +0000

    ^brightness instead of white for pointer + speedup

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -11,28 +11,15 @@ def sort(obj):
         for n in reversed(range(0,sorted)):
             if obj.stripState[n] > obj.stripState[n+1]:
                 audio.out(obj.stripState[n][0])
+
                 mem = obj.stripState[n+1]
-                obj.stripState[n+1] = [0,255,255,255,255]
+                obj.stripState[n+1] = [mem[0],255,mem[2],mem[3],mem[4]]                
+                #obj.stripState[n+1] = [0,255,255,255,255]
                 obj.update()
                 obj.stripState[n+1] = obj.stripState[n]
                 obj.stripState[n] = mem
-                obj.update()
+                #obj.update()
             else:
                 break
         sorted += 1
-        obj.update()
-
-#arr = [3,2,5,9,6,1,8]
-#sorted = 1
-
-#for i in range(0,len(arr)-1):
-#    for n in reversed(range(0,sorted)):
-#        if arr[n] > arr[n+1]:
-#            mem = arr[n]
-#            arr[n] = arr[n+1]
-#            arr[n+1] = mem
-#        else:
-#            break    
-#    sorted+=1 #new item added to sorted arr
-
-#print(arr)
+        #obj.update()
