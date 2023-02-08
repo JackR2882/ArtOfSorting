131800a974d4c7721b3b9e565ef7d055eed3d95c
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-08 09:15:41 +0000

    init thread to watch audio buffer

--- thread_start.py
+++ thread_start.py
@@ -4,9 +4,14 @@
 import main
 import threading
 import speech_recognition
+from init import buffer
+import time
 
 #setup main object, runs the sorting algorithms
 main = main.Main()
+
+# buffer for audio out
+audioBuff = None 
 
 def thread_listen():
     while True: # need to listen in a loop
@@ -14,11 +19,29 @@ def thread_listen():
         main.interrupt(interrupt_val)
 
 def thread_main():
-    main.run()
+    main.run(audioBuff)
+
+def thread_audio_out():
+
+    global audioBuff
+    audioBuff = buffer.Buff()
+
+    import time
+    while True:
+        if len(audioBuff.buffer) > 0:
+            print(str(audioBuff.buffer[0]) + " in buffer")
+            audioBuff.remove()
+
 
 #start threads
 if __name__ == "__main__":
     mainThread = threading.Thread(target=thread_main)    
     interruptThread = threading.Thread(target=thread_listen)
+    audioOutThread = threading.Thread(target=thread_audio_out)
+    audioOutThread.start()
+    # start these fractionally later, to give time for audioObj to be generate
+    print("Initializing...")
+    time.sleep(0.1)
     mainThread.start()
     interruptThread.start()
+
