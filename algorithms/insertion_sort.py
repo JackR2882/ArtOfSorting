c661a0df25a090c5ca6746bfba22882ca3b43370
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-08 11:38:30 +0000

    integrated new technique for audio out

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,16 +1,13 @@
-import audio_controller_temp
-
 #basic implmentation of insertion sort:
-def sort(obj):
 
-    audio = audio_controller_temp.AudioOut()
+def sort(obj, audioBuff):
 
     sorted = 1
     for i in range(0, len(obj.stripState)-1):
-        audio.out(obj.stripState[i][0])
+        audioBuff.append(obj.stripState[i][0])
         for n in reversed(range(0,sorted)):
             if obj.stripState[n] > obj.stripState[n+1]:
-                audio.out(obj.stripState[n][0])
+                #audio.out(obj.stripState[n][0])
 
                 mem = obj.stripState[n+1]
                 obj.stripState[n+1] = [mem[0],255,mem[2],mem[3],mem[4]]                
