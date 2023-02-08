2cfb86205d6b48be758809d6394343dd7068258b
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-08 11:33:10 +0000

    added control to synchronize buffer / algorithm

--- init/buffer.py
+++ init/buffer.py
@@ -1,8 +1,12 @@
+import time
+
 class Buff:
     def __init__(self):
         self.buffer = []
 
     def append(self, item):
+        while (len(self.buffer) > 2):
+                time.sleep(0)
         self.buffer.append(item)
 
     def remove(self):
