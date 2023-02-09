8aae2631a78893d52ada791b0fab7e873f740e6d
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-09 12:01:47 +0000

    modified insertion sort for use in tim sort

---
+++ algorithms/insertion_sort_modified.py
@@ -0,0 +1,25 @@
+# modified implmentation of insertion sort:
+# for use in tim-sort
+# only operates on a slice of the whole array, starting at start and ending at end
+
+def sort(obj, audioBuff, start, end):
+
+    sorted = start+1
+    for i in range(start, end):
+        audioBuff.append(obj.stripState[i][0])
+        for n in reversed(range(start,sorted)):
+            if obj.stripState[n] > obj.stripState[n+1]:
+                audioBuff.append(obj.stripState[i][0])
+
+                mem = obj.stripState[n+1]
+                obj.stripState[n+1] = [mem[0],255,mem[2],mem[3],mem[4]]                
+                #obj.stripState[n+1] = [0,255,255,255,255]
+                obj.update()
+                obj.stripState[n+1] = obj.stripState[n]
+                obj.stripState[n] = mem
+                #obj.update()
+            else:
+                break
+            #obj.update()
+        sorted += 1
+        #obj.update()
