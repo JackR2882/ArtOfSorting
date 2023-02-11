f8f54b1eb8ec4cd315a7cac0b1911a4e2e91a918
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-11 14:57:54 +0000

    optimisiation, pre calculated waveform*=transform

--- audio_controller_temp.py
+++ audio_controller_temp.py
@@ -14,6 +14,8 @@ class AudioOut:
                                   channels = 1,
                                   rate = self.sampleRate,
                                   output = True)
+        
+        self.waveform = (np.sin(2 * np.arange((self.sampleRate/2) * (self.duration/2)) * self.sineFreq / self.sampleRate)).astype(np.float32) # 1 is amplitude value
 
     def cleanup(self):
         self.stream.stop_stream()
@@ -23,34 +25,37 @@ class AudioOut:
     def out(self, val):
         #amp = (val/144+0.5)
         #amp = (val/72+0.5)*np.pi
-        amp = (val/144)
-
-
-
-        ordered = (np.sin(amp * np.arange((self.sampleRate/2) * (self.duration/2)) * self.sineFreq / self.sampleRate)).astype(np.float32)
-        
+        transform = (val/144)
         
         # cut back waveform to the most recent peak - causes waves to mesh better preventing pops
-        n = len(ordered)-1
-        if ordered[len(ordered)-1] > 0: # need to do this when positive (above line)
-            while ordered[n] < ordered[n-1]:
-                ordered = np.delete(ordered, n)
+        
+        waveform = self.waveform.copy()
+        
+        n = len(waveform)-1
+        if waveform[len(waveform)-1] > 0: # need to do this when positive (above line)
+            while waveform[n] < waveform[n-1]:
+                waveform = np.delete(waveform, n)
                 n-=1
-        if ordered[len(ordered)-1] < 0: # and negative (below line)
-            while ordered[n] > ordered[n-1]:
-                ordered = np.delete(ordered, n)
+        if waveform[len(waveform)-1] < 0: # and negative (below line)
+            while waveform[n] > waveform[n-1]:
+                waveform = np.delete(waveform, n)
                 n-=1
 
         
-        ramp = np.arange(0,1,1/len(ordered))
-        while len(ordered) < len(ramp):
+        ramp = np.arange(0,1,1/len(waveform))
+        while len(waveform) < len(ramp):
             ramp = np.delete(ramp, len(ramp)-1)
-        ordered *= ramp
-
-        ordered = np.concatenate((ordered, np.flip(ordered)), axis=None)
+        waveform *= ramp
 
 
-        output = (self.volume * ordered).tobytes()
+        # need to now apply the transform to waveform:
+        waveform *= transform
+
+
+        waveform = np.concatenate((waveform, np.flip(waveform)), axis=None)
+
+
+        output = (self.volume * waveform).tobytes()
 
         self.stream.write(output)
 
