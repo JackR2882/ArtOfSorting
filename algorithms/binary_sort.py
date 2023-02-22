951689a6e8846de71cb3aeeda6f596ef3e32492b
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-22 14:27:19 +0000

    changed to stacking highlight + refactoring

--- algorithms/binary_sort.py
+++ algorithms/binary_sort.py
@@ -1,14 +1,13 @@
 #basic implmentation of binary sort:
 #essentially the same as insertion sort, however a binary search is used to find the correct index for insertion
 
+
+
 def binary_search(val, obj, start_i, end_i, default_b):
 
     # highlight current section of arr
-    obj.highlight(start_i, end_i, default_b)
-
-    #import time
-    #time.sleep(0.1)
-
+    # can switch between stacking and non-stacking brightness change by enabling / unenabling the flag
+    obj.highlight(start_i, end_i, default_b, stack=True)
 
     arr = obj.stripState
 
@@ -29,23 +28,11 @@ def binary_search(val, obj, start_i, end_i, default_b):
 
     return(i)
 
-#def insert(arr, insertion_index, val):
-  
-#    new_arr = [0]*(len(arr)+1)
-
-    #print("inserting, " + str(val) + " @: " + str(insertion_index))
-
-#    new_arr[0:insertion_index] = arr[0:insertion_index]
-#    new_arr[insertion_index] = val
-#    new_arr[insertion_index+1:len(new_arr)] = arr[insertion_index:len(arr)]
-
-#    return(new_arr)
 
 
 def sort(obj, audioBuff):
 
     sorted = 1
-    sorted_arr = obj.stripState[0:sorted]
 
     default_b = obj.stripState[0][1]
 
@@ -61,15 +48,13 @@ def sort(obj, audioBuff):
         index = binary_search(val, obj, 0, sorted, default_b) # perform binary search to find where to insert item
         #sorted_arr = insert(sorted_arr, index, val) # insert item into sorted arr at given index (need to shift anything to the right of including the index, right by one)
 
+        obj.highlight(0,0,default_b)
+
         # possible way of inserting -> keeps execution time in line with other algorithms but doesn't look the best
         prev = obj.stripState[index]
         obj.stripState[index] = val
         for n in range(index+1, sorted+1):
             obj.stripState[n], prev = prev, obj.stripState[n]
             obj.update()
-
-        #obj.stripState[0:len(sorted_arr)] = sorted_arr # update arr
-
-        #obj.update()
 
         sorted += 1
