1d9c239a6d176f41742728ce30c319d9ae8aa1e3
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-22 19:07:24 +0000

    fixed heapify, but broken sorting

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -15,39 +15,63 @@ import math
 # n is size of heap
 
 
-
-# seems to work but is relatively slow
-def heapify(arr_in, i):
-
-	parent = math.floor(((i+1)/2)-1)
-
-	if i >= 0:
-
-		x = i
-		# bubble up as far as possible
-		while parent >= 0:			
-			if (arr_in[parent] < arr_in[x]):
-
-				# swap (bubble up)
-				arr_in[x], arr_in[parent] = arr_in[parent], arr_in[x]
-
-				arr_in = heapify(arr_in, x)
-
-				x = parent
-
-			parent = math.floor(((parent+1)/2)-1)
-
-		# move to next item
-		i -= 1
-		arr_in = heapify(arr_in, i)
+def heapify2(arr, n, obj, default_b):
 	
-	return arr_in
+	#parent = int((n/2)-1)
+	parent = n
+
+	obj.highlight(0,0,225) # clear previous higlighting
+    
+	child_left = 2*parent+1
+	child_right = 2*parent+2
+
+	#obj.highlight(parent, parent+1, default_b, stack=True, val=10)
+	#obj.highlight(child_left, child_right+1, default_b, stack=True, val=10)
+	#obj.highlight(child_right, child_right+1, default_b, stack=True, val=10)
+    
+	# compare with children
+	if (child_left < len(arr)) and (arr[child_left] > arr[parent]):
+		# swap left child and parent
+		arr[child_left], arr[parent] = arr[parent], arr[child_left]
+		arr = heapify2(arr, n, obj, default_b)
+	if (child_right < len(arr)) and (arr[child_right] > arr[parent]):
+	    # swap right child and parent
+		arr[child_right], arr[parent] = arr[parent], arr[child_right]
+		arr = heapify2(arr, n, obj, default_b)
+	#else:
+		#n-=1
+		#if n > 0:
+		#	arr = heapify2(arr, n, obj, default_b)
+		#if parent > 0:
+		#	arr = heapify2(arr, parent, obj, default_b)
+
+	return(arr)
+
+
+def heap(arr, obj, default_b):
+	for n in range(math.floor(len(arr)/2), 0, -1):
+		arr = heapify2(arr, n-1, obj, default_b)
+	return(arr)
+
 
 def sort(obj):
+
+	default_b = obj.stripState[0][1]
+
 	arr = obj.stripState
 
-	obj.stripState = heapify(arr,len(arr)-1)
-	obj.update()
+
+	#for n in range(math.floor(len(arr)/2), 0, -1):
+	#	obj.stripState = heapify2(arr, n-1, obj, default_b)
+	#	obj.update()
+	obj.stripState = heap(arr, obj, default_b)
+
+
+	import time
+	print("heaped: " + str(obj.stripState[0][0]))
+	#time.sleep(500)
+	# could be issues with direction of sorting -> min/max heap ?
+
 
 	heaped_arr = obj.stripState
 	sorted_arr = [[0,0,0,0,0]]*len(heaped_arr)
@@ -58,10 +82,15 @@ def sort(obj):
 
 		sorted_arr[i] = heaped_arr[0]
 		heaped_arr[0], heaped_arr[len(heaped_arr)-(1+i)] = heaped_arr[len(heaped_arr)-(1+i)], [0,0,0,0,0]
-		heaped_arr = heapify(heaped_arr, len(heaped_arr)-1)
+		
+		heap(heaped_arr, obj, default_b)
+		heaped_arr = obj.stripState
+		#heaped_arr = heapify2(heaped_arr, len(heaped_arr)-1, obj, default_b)
 
-		obj.stripState = sorted_arr
+		#obj.stripState = sorted_arr
 		obj.update()
+
+		print("heaped: " + str(obj.stripState[0][0]))
 
 		i += 1
 
