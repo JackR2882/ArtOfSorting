83b189795fd600824f2c1fd5205ab99d1f0b4db5
JackR223 <sgjrive@liverpool.ac.uk>
2023-02-22 09:42:57 +0000

    switched to counting sort

--- algorithms/radix_sort.py
+++ algorithms/radix_sort.py
@@ -1,8 +1,6 @@
 # basic implmentation of radix sort:
 
 # use counting sort to sort array based on current digit
-# CURRENTLY WORKING WITH INSERTION SORT WHICH IS ALLOWED AS IT IS A STABLE SORTING METHOD, HOWEVER SHOULD PROBABLY SWITCH
-# TO COUNTING SORT IF POSSIBLE
 
 # need to work out maximum item in array to inform radix sort, then if item is for example 146: sort
 # items first by their hundreth digit value, e.g. 146 == 1 and 95 == 0
@@ -10,7 +8,6 @@
 
 
 import math
-from algorithms import selection_sort_modified as selection_sort
 from algorithms import counting_sort_modified as counting_sort
 
 
@@ -30,5 +27,7 @@ def sort(obj):
     off = int(math.pow(10,size-1))
 
     while off >= 1:
-        selection_sort.sort(obj, off)
+        counting_sort.sort(obj, off)
         off = off/10
+
+    print(obj.stripState)
