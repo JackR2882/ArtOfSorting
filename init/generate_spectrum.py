69a912c23601bc3bb1f819211aad827f87e4aeea
JackR223 <sgjrive@liverpool.ac.uk>
2023-03-01 16:24:52 +0000

    removed unecessary argument from initalize

--- init/generate_spectrum.py
+++ init/generate_spectrum.py
@@ -13,12 +13,12 @@ def initialize(obj, stripSize, defaultBrightness):
     step = math.floor(255/rampSize)
     i = 0
     while i < rampSize:        
-        obj.setPixel(i,i,defaultBrightness, 255,i*step,0)
-        obj.setPixel(i+rampSize,i+rampSize,defaultBrightness,255-i*step,255,0)
-        obj.setPixel(i+rampSize*2,i+rampSize*2,defaultBrightness,0,255,i*step)
-        obj.setPixel(i+rampSize*3,i+rampSize*3,defaultBrightness,0,255-i*step,255)
-        obj.setPixel(i+rampSize*4,i+rampSize*4,defaultBrightness,i*step,0,255)
-        obj.setPixel(i+rampSize*5,i+rampSize*5,defaultBrightness,255,0,255-i*step)
+        obj.setPixel(i,defaultBrightness, 255,i*step,0)
+        obj.setPixel(i+rampSize,defaultBrightness,255-i*step,255,0)
+        obj.setPixel(i+rampSize*2,defaultBrightness,0,255,i*step)
+        obj.setPixel(i+rampSize*3,defaultBrightness,0,255-i*step,255)
+        obj.setPixel(i+rampSize*4,defaultBrightness,i*step,0,255)
+        obj.setPixel(i+rampSize*5,defaultBrightness,255,0,255-i*step)
         i+=1
 
 
