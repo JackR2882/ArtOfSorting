9e0346558604dc369455eef215bc0560bef95d3e
JackR223 <sgjrive@liverpool.ac.uk>
2023-03-18 16:08:27 +0000

    switched from audioBuff to audioObj

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,6 +1,6 @@
 # basic implementation of bubble sort:
 
-def sort(obj, audioBuff):
+def sort(obj, audioObj):
 
     unsorted = True
     sorted = 0 # optimisation as algoithm will always put largest item on the right (don't need to re-check this area each time)
@@ -10,7 +10,9 @@ def sort(obj, audioBuff):
 
         for i in range(1, (len(obj.stripState)-sorted)):
 
-            audioBuff.append(obj.stripState[i][0])
+            #audioBuff.append(obj.stripState[i][0])
+            audioObj.update(obj.stripState[i][0])
+            #audioObj.out(obj.stripState[i][0])
 
             currVal = obj.stripState[i].copy()
             prevVal = obj.stripState[i-1].copy()
