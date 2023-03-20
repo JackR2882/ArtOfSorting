d78876cb01f81af2f77110e2a13fc6273a927862
JackR223 <sgjrive@liverpool.ac.uk>
2023-03-20 11:23:08 +0000

    integrated new hotowrd detection files

--- speech_recognition.py
+++ speech_recognition.py
@@ -26,6 +26,9 @@ porcupine = pvporcupine.create(access_key = access_key, keyword_paths=["/home/pi
                                                                        "/home/pi/ArtOfSorting/hotword_detection/counting_sort/counting-sort_en_raspberry-pi_v2_1_0.ppn",
                                                                        "/home/pi/ArtOfSorting/hotword_detection/bucket_sort/bucket-sort_en_raspberry-pi_v2_1_0.ppn",
                                                                        "/home/pi/ArtOfSorting/hotword_detection/radix_sort/radix-sort_en_raspberry-pi_v2_1_0.ppn",
+                                                                       "/home/pi/ArtOfSorting/hotword_detection/cocktail_sort/cocktail-sort_en_raspberry-pi_v2_1_0.ppn",
+                                                                       "/home/pi/ArtOfSorting/hotword_detection/tim_sort/tim-sort_en_raspberry-pi_v2_1_0.ppn",
+                                                                       "/home/pi/ArtOfSorting/hotword_detection/binary_sort/binary-sort_en_raspberry-pi_v2_1_0.ppn",
                                                                        "/home/pi/ArtOfSorting/hotword_detection/slow_mode/slow-mode_en_raspberry-pi_v2_1_0.ppn",
                                                                        "/home/pi/ArtOfSorting/hotword_detection/fast_mode/fast-mode_en_raspberry-pi_v2_1_0.ppn"
                                                                        ])
@@ -77,7 +80,7 @@ def listen():
             # if hotword detected, then which hotword is detected?
             hotwords = ["bubble", "insertion", "merge", "selection",
                         "heap", "quick", "counting", "bucket", "radix",
-                        "slow", "fast"]
+                        "cocktail", "tim", "binary", "slow", "fast"]
             
             output = [hotwords[hotword], vol]
             
@@ -99,3 +102,25 @@ def listen():
 
 # FIXED BY CHANGING FILE: nano ~/.asoundrc
 # using this guide: https://matthewdaws.github.io/blog/2019-12-07-pi-audio.html
+
+#  GNU nano 5.4                                                           /home/pi/.asoundrc                                                                    
+#pcm.!default {
+#type asym
+#playback.pcm {
+#    type plug
+#    slave.pcm "hw:0,0"
+#}
+#capture.pcm {
+#    type plug
+#    slave.pcm "hw:1,0"
+#}
+#}
+
+
+#make unmutable:
+#https://www.tecmint.com/make-file-directory-undeletable-immutable-in-linux/#:~:text=To%20make%20a%20file%20mutable,the%20above%20attribute%2C%20as%20follows.&text=You%20will%20find%20these%20related,Enabling%20sudo%20Access%20on%20Users
+#sudo chattr +i ~/.asoundrc
+
+
+#make mutable:
+#sudo chattr -i ~/.asoundrc
