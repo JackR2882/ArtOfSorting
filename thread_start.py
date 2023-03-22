52e710fc0902527e1f4d0552cc22947f893f734a
JackR223 <sgjrive@liverpool.ac.uk>
2023-03-22 11:44:28 +0000

    trimmed down running threads to three

--- thread_start.py
+++ thread_start.py
@@ -5,14 +5,9 @@ import main
 import threading
 import speech_recognition
 import audio_controller
-import display_controller
-import time
 
 #setup main object, runs the sorting algorithms
 main = main.Main()
-
-# buffer for audio out
-audioBuff = None
 
 # buffer for display out
 displayObj = None
@@ -29,34 +24,20 @@ def thread_listen():
 def thread_main():
     main.run(audioObj, displayObj)
 
-def thread_out():
-    # mights as well define both output objects together
-    global audioObj
-    audioObj = audio_controller.AudioOut()
-    global displayObj
-    displayObj = display_controller.Display()
-
-    #while True:
-    #    if displayObj.changed:
-    #        displayObj.update()
-
-
-
 #start threads
 if __name__ == "__main__":
 
+    # define audio output object
+    global audioObj
+    audioObj = audio_controller.AudioOut()
+
+    # define + start main and interrupt threads
     mainThread = threading.Thread(target=thread_main)    
     interruptThread = threading.Thread(target=thread_listen)
-    outThread = threading.Thread(target=thread_out)
-    outThread.start()
-
-    # start these fractionally later, to give time for audio and LED objects to be generated
-    print("Initializing...")
-    time.sleep(2)
-
-    audioOutThread = threading.Thread(target=audioObj.audio_out)
-    audioOutThread.start()
-
     mainThread.start()
     interruptThread.start()
+
+    # define + start audio output thread
+    audioOutThread = threading.Thread(target=audioObj.audio_out)
+    audioOutThread.start()
 
