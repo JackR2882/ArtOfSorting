8785ba383135e5a1bb32b1f92bb869d1ff804e06
JackR223 <sgjrive@liverpool.ac.uk>
2023-03-27 11:42:53 +0100

    clean up + switched to highlighing method

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -2,6 +2,8 @@
 
 def sort(obj, audioObj):
 
+    default_b = obj.stripState[0][1]
+
     unsorted = True
     sorted = 0 # optimisation as algoithm will always put largest item on the right (don't need to re-check this area each time)
 
@@ -10,31 +12,18 @@ def sort(obj, audioObj):
 
         for i in range(1, (len(obj.stripState)-sorted)):
 
-            #audioBuff.append(obj.stripState[i][0])
             audioObj.update(obj.stripState[i][0])
-            #audioObj.out(obj.stripState[i][0])
 
             currVal = obj.stripState[i].copy()
             prevVal = obj.stripState[i-1].copy()
-            #obj.stripState[i] = [0,255,255,255,255]
-            #obj.stripState[i-1] = [0,255,255,255,255]
-            obj.stripState[i][1] += 10
-            obj.stripState[i-1][1] += 10
+            obj.highlight(i-1,i+1, default_b)
+            #obj.stripState[i][1] += 10
+            #obj.stripState[i-1][1] += 10
             obj.update()
-
  
             obj.stripState[i], obj.stripState[i-1] = currVal, prevVal # reset brightness
 
             if obj.compareAndSwapPixel(i,i-1):
                 unsorted = True
-
-            #if currVal < prevVal:
-                # swap values
-            #    obj.swapPixel(i,i-1)
-            #    unsorted = True
-            #else:
-            #    # just reset brightness 
-            #    obj.stripState[i], obj.stripState[i-1] = currVal, prevVal 
-                
 
         sorted += 1
