1558ec3dce554638716f5c5159c95088cbfa1c70
JackR223 <sgjrive@liverpool.ac.uk>
2023-04-04 08:48:29 +0100

    reformatted display + fixed spacing

--- display_controller.py
+++ display_controller.py
@@ -1,14 +1,12 @@
 # controller to handle viusal output to display
-
 # SCREEN DIMENSIONS: 800x480
 
 import tkinter
-from tkinter import ttk
 import os
+import customtkinter as ctk
 
 
 # set correct display
-
 if os.environ.get('DISPLAY','') == '':
     os.environ.__setitem__('DISPLAY', ':0.0')
 
@@ -16,10 +14,12 @@ if os.environ.get('DISPLAY','') == '':
 class Display:
     def __init__(self):
 
+        ctk.set_appearance_mode("Dark")
+
         # initalize window
-        self.root = tkinter.Tk()
-        #self.root.geometry('800x480')
-        self.frm = ttk.Frame(self.root, padding=10)
+        self.root = ctk.CTk()
+
+        self.frm = ctk.CTkFrame(self.root)
         self.frm.grid()
         
         self.root.wm_attributes('-fullscreen', 'True')  # enable later -> will stretch window to fit fullscreen
@@ -29,36 +29,35 @@ class Display:
         self.root.config(cursor="none")
 
         # initalize relevant variables + outputs
-        self.currAlg = "long algorithm name here"
-        self.currAlgLbl = ttk.Label(self.frm, text="Current algorithm: " + self.currAlg, font=("Times", 40), wraplength=790)
-        self.currAlgLbl.grid(column=0, row=0, columnspan=6, rowspan=3)
+        self.currAlg = "placeholder"
+        self.currAlgLbl = ctk.CTkLabel(self.root, text="Current algorithm: " + self.currAlg, font=("Times", 60), wraplength=790)
+        self.currAlgLbl.place(relx=0.0, rely=0.1, anchor=tkinter.W)
 
-        self.swapSD = 0.0 # these two values need to be slightly different otherwise tkinter treats them as the same and the sliders 'stick' together
-        self.compareSD = 0.001
-        self.swapSDLbl = ttk.Label(self.frm, text="Swap SD: " + str(self.swapSD), justify='left', font=('Times', 25), width=20)
-        self.swapSDLbl.grid(column=0, row=6, rowspan=3) # need to do this separately as .grid() returns none
-        self.compareSDLbl = ttk.Label(self.frm, text="Comparison SD: " + str(self.compareSD), justify='left', font=("Times", 25), width=20)
-        self.compareSDLbl.grid(column=0, row=10, rowspan=3)
-        self.sExplainLbl = ttk.Label(self.frm, text="Slow down is performed on an item swap in array, that is, whenever an item changes position.", wraplength=300, justify="left", font=("Times", 15))
-        self.sExplainLbl.grid(column=3, row=6, rowspan=3)
-        self.cExplainLbl = ttk.Label(self.frm, text="Slow down is performed on an item comparison, that is, whenever two items are compared.", wraplength=300, justify="left", font=("Times", 15))
-        self.cExplainLbl.grid(column=3, row=10, rowspan=3)
+        self.swapSD = tkinter.DoubleVar(0.0)
+        self.compareSD = tkinter.DoubleVar(0.0)
+
+        self.swapSDLbl = ctk.CTkLabel(self.root, text="Swap SD: " + str(self.swapSD), justify='left', font=('Times', 39), width=20)
+        self.swapSDLbl.place(relx=0.0, rely=0.4, anchor=tkinter.W)
+        self.compareSDLbl = ctk.CTkLabel(self.root, text="Comparison SD: " + str(self.compareSD), justify='left', font=("Times", 39), width=20)
+        self.compareSDLbl.place(relx=0.0, rely=0.6, anchor=tkinter.W)
+        self.sExplainLbl = ctk.CTkLabel(self.root, text="Slow down is performed on an item swap in array, that is, whenever an item changes position.", wraplength=390, justify="left", font=("Times", 25))
+        self.sExplainLbl.place(relx=0.5, rely=0.4, anchor=tkinter.W)
+        self.cExplainLbl = ctk.CTkLabel(self.root, text="Slow down is performed on an item comparison, that is, whenever two items are compared.", wraplength=390, justify="left", font=("Times", 25))
+        self.cExplainLbl.place(relx=0.5, rely=0.6, anchor=tkinter.W)
 
         # initialize and display sliders for swap and comparison slowdowns
-        self.swapSlider = ttk.Scale(self.frm, from_=0, to=0.5, orient='horizontal', variable=self.swapSD, length=300)
-        self.swapSlider.grid(column=0, row=9)
-        self.compareSlider = ttk.Scale(self.frm, from_=0, to=0.5, orient='horizontal', variable=self.compareSD, length=300)
-        self.compareSlider.grid(column=0, row=13)
+        self.swapSlider = ctk.CTkSlider(self.root, from_=0, to=0.5, orientation='horizontal', variable=self.swapSD, width=385, number_of_steps=20)
+        self.swapSlider.place(relx=0.0, rely=0.5, anchor=tkinter.W)
+        self.compareSlider = ctk.CTkSlider(self.root, from_=0, to=0.5, orientation='horizontal', variable=self.compareSD, width=385, number_of_steps=20)
+        self.compareSlider.place(relx=0.0, rely=0.7, anchor=tkinter.W)
 
-        self.nextAlg = "long algorithm name here"
-        self.nextAlgLbl = ttk.Label(self.frm, text="Next algorithm: " + str(self.nextAlg), justify='left', font=("Times", 25))
-        self.nextAlgLbl.grid(column=0, row=15, columnspan=4)
+        self.nextAlg = "placeholder"
+        self.nextAlgLbl = ctk.CTkLabel(self.root, text="Next algorithm: " + str(self.nextAlg), justify='left', font=("Times", 39))
+        self.nextAlgLbl.place(relx=0.0, rely=0.9, anchor=tkinter.W)
 
 
 
     def change(self, update):
-        
-        #print("updating: " + update[0] + " & " + update[1])
 
         if update[0]:
             self.currAlg = update[0]
@@ -70,14 +69,14 @@ class Display:
     def refresh(self):
         while True:
             # update label text values
-            self.currAlgLbl.config(text="Current algorithm: " + self.currAlg)
-            self.swapSDLbl.config(text="Swap SD: " + str(self.swapSD))
-            self.compareSDLbl.config(text="Comparison SD: " + str(self.compareSD))
-            self.nextAlgLbl.config(text="Next algorithm: " + str(self.nextAlg))
+            self.currAlgLbl.configure(text="Current algorithm: " + self.currAlg)
+            self.swapSDLbl.configure(text="Swap SD: " + str(self.swapSD))
+            self.compareSDLbl.configure(text="Comparison SD: " + str(self.compareSD))
+            self.nextAlgLbl.configure(text="Next algorithm: " + str(self.nextAlg))
 
             # update values based on current slider position
-            self.swapSD = self.swapSlider.get()
-            self.compareSD = self.compareSlider.get()
+            self.swapSD = round(self.swapSlider.get(),2)
+            self.compareSD = round(self.compareSlider.get(),2)
 
             # refresh screen with new values
             self.root.update()
