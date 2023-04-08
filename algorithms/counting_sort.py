bf7f0797e67e8f71f0ccdfc9a28e6a9b76cde7cb
JackR223 <sgjrive@liverpool.ac.uk>
2023-04-08 16:51:32 +0100

    fixed issue with entire array not being sorted

--- algorithms/counting_sort.py
+++ algorithms/counting_sort.py
@@ -8,32 +8,26 @@ import time
 
 def sort(obj):
 
-    swapSD = obj.swapSD
-    compareSD = obj.compareSD
+
 
     arr = obj.stripState
 
-    #def count(arr, item):
-    #    counter = 0
-    #    for i in range(0, len(arr)):
-    #        #time.sleep(compareSD) # this is technically the correct delay - but doesn't work well
-    #        if arr[i][0] == item:
-    #            counter += 1
-
-    #    time.sleep(compareSD)
-    #    return counter
 
 
     # find max item in array:
     max_i = 0
     for i in range(1, len(arr)):
-        time.sleep(compareSD)
+        time.sleep(obj.compareSD)
         if arr[max_i] < arr[i]:
             max_i = i
+        time.sleep(obj.compareSD)
+
 
 
     # init arr of length max + 1
-    new_arr = [0]*(int(arr[max_i][0])+1)
+    #new_arr = [0]*(int(arr[max_i][0])+1)
+    new_arr = [0]*(len(arr)-1)
+
 
 
     # clear array, before frequency count is to be displayed
@@ -41,17 +35,20 @@ def sort(obj):
     obj.stripState = [[0,224,0,0,0]]*146
 
 
+
     # work out and display frequencies (count items)
     # raw count:
     for i in range(0, len(new_arr)):
-        time.sleep(compareSD)
-
         new_arr[arr[i][0]] += 1 # update with raw count
-
+        
         # display frequencies as the colour white on the led strip: -> frequency of pixel corresponds to the brightness of its pixel
         ratio = (new_arr[arr[i][0]]) / 30
         obj.stripState[arr[i][0]] = [i,int(255 - (12*ratio)),int(255*ratio),int(255*ratio),int(255*ratio)]
         obj.update()
+        
+        time.sleep(obj.swapSD) # technically updating array, so count as swap
+
+    print(new_arr)
 
 
 
@@ -59,7 +56,6 @@ def sort(obj):
     # cumulative-count:
     acc = 0
     for i in range(0, len(new_arr)):
-        time.sleep(compareSD)
         acc += new_arr[i]
         new_arr[i] = acc
 
@@ -71,37 +67,33 @@ def sort(obj):
             obj.stripState[i] = [i,0,255,255,255]
         obj.update()
 
+        time.sleep(obj.swapSD) # technically updating array, so count as swap
+
 
     
     # revert strip state back to initial state
-    sorted_arr = obj.stripState
     obj.stripState = arr.copy()
     obj.update()
 
 
-    # sort items:
+
     for i in range(0, len(arr)):
-        time.sleep(compareSD)
+        index = new_arr[int(arr[i][0])]
+        prev_index = new_arr[int(arr[i][0])-1]
 
-        index = new_arr[int(arr[i][0])]-1
+        # need to account for repeated items:
+        if prev_index > index or prev_index == 0:
+            prev_index = -1
 
-        sorted_arr[index] = arr[i]
+        for d in range(index, prev_index, -1):
+            obj.stripState[d] = arr[i]
+            obj.update()
 
 
-        # need to deal with multiple items overlapping:
-        try:
-            n = (new_arr[(arr[i][0])]) - (new_arr[(arr[i][0])-1])
-            #print("1: " + str(new_arr[int(arr[i][0])])+ ", 2: " + str(new_arr[int(arr[i][0])-1]))
-            #n = (146 - index)-1        
-        except:
-            n = index
 
-        #while n > 0:
-        #    sorted_arr[index-n] = arr[i]
-        #    obj.stripState[index-n] = arr[i]
-        #    n -= 1
-        #    obj.update()
-
-    print("this is broken - need to fix ^")
+    # index:        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    # arr:          1, 1, 2, 1, 3, 2, 4, 1, 2, 0
+    # raw:          1, 4, 3, 1, 1, 0, 0, 0, 0, 0
+    # cumulative:   1, 5, 8, 9,10,10,10,10,10,10
 
 
