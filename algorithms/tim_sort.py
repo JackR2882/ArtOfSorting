890fa324d589a2cbcf2fde48e1bfaaccd0f72517
JackR223 <sgjrive@liverpool.ac.uk>
2023-04-10 10:28:19 +0100

    minor changes to slowdown placement

--- algorithms/tim_sort.py
+++ algorithms/tim_sort.py
@@ -2,14 +2,14 @@
 
 # split array into runs (size 32)
 # then use insertion sort on those runs
-# and merge runs using the merge from merge sort
+# then merge runs using the merge from merge sort
 
 from algorithms import insertion_sort_modified as insertion_sort
 import math
 import time
 
 
-# try and find run size between max run size and 0 that will allow the arr to be equally divided into runs
+# try and find run size between max run size and 0 that will allow the arr to be equally divided into runs UNUSED
 def calculate_run_size(max_run_size, length):
     run_size = max_run_size
     while max_run_size > 0:
@@ -23,11 +23,8 @@ def calculate_run_size(max_run_size, length):
 
 def merge(obj, audioObj, start_l, end_l, start_r, end_r):
 
-    swapSD = obj.swapSD
-    compareSD = obj.compareSD
-    
-    #print("start_l: " + str(start_l) + ", end_l: " + str(end_l))
-    #print("start_r: " + str(start_r) + ", end_r: " + str(end_r))
+    #swapSD = obj.swapSD
+    #compareSD = obj.compareSD
 
     offset = start_l
 
@@ -36,42 +33,40 @@ def merge(obj, audioObj, start_l, end_l, start_r, end_r):
     
     while(len(l_arr) and len(r_arr)) > 0:
 
-        time.sleep(compareSD + swapSD)
-
         if l_arr[0] > r_arr[0]:
             obj.stripState[offset] = r_arr[0]
-
             r_arr.pop(0)
         else:
             obj.stripState[offset] = l_arr[0]
-
             l_arr.pop(0)
-            
+        
         audioObj.update(obj.stripState[offset][0])
+
+        time.sleep(obj.compareSD + obj.swapSD)
 
         offset += 1
         obj.update()
 
     while len(r_arr) > 0:
 
-        time.sleep(swapSD)
-
         obj.stripState[offset] = r_arr[0]
         r_arr.pop(0)
 
         audioObj.update(obj.stripState[offset][0])
+
+        time.sleep(obj.swapSD)
 
         offset += 1
         obj.update()
 
     while len(l_arr) > 0:
 
-        time.sleep(swapSD)
-
         obj.stripState[offset] = l_arr[0]
         l_arr.pop(0)
-        
+
         audioObj.update(obj.stripState[offset][0])
+
+        time.sleep(obj.swapSD)
         
         offset += 1
         obj.update()
@@ -86,8 +81,6 @@ def sort(obj, audioObj):
     # don't need to split arr into runs, can instead just use slices of arr
     # (runs still exist, just aren't represented)
     
-    max_run_size = 32
-    #run_size = calculate_run_size(max_run_size, len(obj.stripState))
     run_size = 32
 
     run_count = math.ceil(len(arr)/run_size)
@@ -121,9 +114,9 @@ def sort(obj, audioObj):
         #      merge left and right sorted partitions
         # 
         # otherwise if right_sorted <= run_count:
-        #      merge right sorted with whatever's to its right 
+        #      merge right sorted with whatever's to its right
+        #  
         if (right_sorted < run_count) and ((left_sorted != (right_sorted-left_sorted)) or left_sorted == 0):
-            #print("HERE1;       left_s: " + str(left_sorted) + ", right_s: " + str(right_sorted))
             inc = min(i+2, run_count)
             
             obj.highlight((i)*run_size, (inc*run_size)-1, default_b)
@@ -136,7 +129,6 @@ def sort(obj, audioObj):
             else:
                 right_sorted = i
         else:
-            #print("HERE2;       left_s: " + str(left_sorted) + ", right_s: " + str(right_sorted))
             obj.highlight(0, (right_sorted*run_size)-1, default_b)
             merge(obj, audioObj, 0, (run_size*left_sorted)-1, (run_size*left_sorted), (run_size*(right_sorted))-1)
             left_sorted = right_sorted
