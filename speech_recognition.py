c6f9d0d9894c59979cc73bebed28ec4dd29fc1e0
JackR223 <sgjrive@liverpool.ac.uk>
2023-04-11 11:00:41 +0100

    refactoring

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,5 +1,5 @@
-# code was inspired by the following resource, in porcupine documentation
-# https://github.com/Picovoice/porcupine/issues/255#issuecomment-876974791
+# inspired by the following resource:
+#  - https://github.com/Picovoice/porcupine/issues/255#issuecomment-876974791
 
 import os
 import pyaudio
@@ -37,7 +37,7 @@ porcupine = pvporcupine.create(access_key = access_key, keyword_paths=["/home/pi
 
 def listen():
 
-    #print("LISTENING "*3)
+    print("LISTENING "*3)
 
     # open audio stream
     stream = listener.open(
@@ -46,14 +46,6 @@ def listen():
         format=pyaudio.paInt16,
         input=True,
         frames_per_buffer=porcupine.frame_length)
-    
-    #prev_amp = [0]*20   # buffer of size 20 to hold previous amplitude val's
-                        # could experiment with increasing this to get more of
-                        # the sound wave
-                        # as it is it seems to catch at least the second word
-                        # more experimentation might be needed
-
-    output = ""
 
     while True:
         wave = stream.read(porcupine.frame_length, exception_on_overflow=False)
@@ -61,44 +53,24 @@ def listen():
 
         # convert to more managable form (byte to decimal)
         wave = struct.unpack_from("h" * porcupine.frame_length, wave)
-        #wave = struct.unpack_from("h" * 512, wave)
 
         # generate value corresponding to what noise is detected (hotword or not, and if hotword then which hotword)
         hotword = porcupine.process(wave)
-        
-        # calculate amplitude of sound wave
-        # amplitude is approximated as the furthest deviation from the zero line at any given point in the sound wave
-        # could improve this to filter out peaks slightly with an average
-        #amp = abs(0 - wave[0])
-        #for i in range(1, len(wave)):
-        #    if abs(0 - wave[i]) > amp:
-        #        amp = abs(0 - wave[i])
 
 
         # if hotword is detected
         if hotword != -1:
-
-            #vol = max(prev_amp) # approximate volume as maximum value from the amplitude buffer
 
             # if hotword detected, then which hotword is detected?
             hotwords = ["bubble", "insertion", "merge", "selection",
                         "heap", "quick", "counting", "bucket", "radix",
                         "cocktail shaker", "tim", "binary", "shell", "slow", "fast"]
             
-            output = [hotwords[hotword]]
+            output = hotwords[hotword]
             
             stream.close()
             return(output)
 
-        # update the buffer with the new amplitude val
-        #prev_amp.pop(0)
-        #prev_amp.append(amp)
-
-
-
-# NEED TO GET AMPLITUDE OF WAVE AS VOLUME LEVEL, CAN USE THIS TO THRESHOLD WHEN HOTWORD DETECTION IS ACTIVE
-# (REDUCING COMPUTATIONAL COST), AND ALSO AS ANOTHER MEANS OF INTERACTING WITH THE SYSTEM
-# can't be used to reduce computational cost - since it's costly in and of itself
 
 
 
