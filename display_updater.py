7ca52f001fd42a801e66f87a081f7bd3f0003fdc
JackR223 <sgjrive@liverpool.ac.uk>
2023-04-23 14:56:14 +0100

    refactoring

--- display_updater.py
+++ display_updater.py
@@ -9,8 +9,8 @@ class DisplayUpdater():
         self.recievePipe = None
         self.sendPipe = None
 
+    # send updated labels to ui
     def send(self, currAlg=None, nextAlg=None):
-        #print("sending: " + currAlg + " & " + nextAlg)
         self.sendPipe.send([currAlg, nextAlg])
 
     # recive updated slowdowns from ui
